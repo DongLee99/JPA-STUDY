663fa0e92df7bdcdd5574509528ba63a203e30b9
DongLee99 <jhnj741@naver.com>
2021-04-03 16:57:59 +0900

    맴버 서비스 구현

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor // final 인 필드만 생성자 인젝션
+public class MemberService {
+    // 회원 가입, 회원 전체 조회
+    private final MemberRepository memberRepository;
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getUsername());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+
+    }
+
+}
+
