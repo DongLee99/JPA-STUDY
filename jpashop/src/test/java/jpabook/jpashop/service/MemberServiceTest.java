dd18154de964776a4b23ad2326216b5d4aefd420
DongLee99 <jhnj741@naver.com>
2021-04-04 15:07:07 +0900

    멤버 서비스 테스트 : 중복 회원 테스트

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
 
@@ -23,8 +26,11 @@ public class MemberServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    EntityManager em;
+
     @Test
-    @Rollback(false)
+    //@Rollback(false)
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
@@ -34,16 +40,26 @@ public class MemberServiceTest {
         Long savedId = memberService.join(member);
 
         //then
+        em.flush();
         assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복확인() throws Exception {
         //given
+        Member member = new Member();
+        member.setUsername("kim1");
+
+        Member member1 = new Member();
+        member1.setUsername("kim1");
 
         //when
+        memberService.join(member1);
+        memberService.join(member);
 
         //then
+        fail("에러 발생");
+
     }
 
 }
