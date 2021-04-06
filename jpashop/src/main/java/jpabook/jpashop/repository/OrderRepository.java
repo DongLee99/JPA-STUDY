276d09afdb46cd335e076ac49cf708b52cf20bd0
DongLee99 <jhnj741@naver.com>
2021-04-06 21:14:59 +0900

    주문 검색 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,5 +24,26 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> findAll(OrderSearch orderSearch){}
+    /*public List<Order> findAll(OrderSearch orderSearch){
+        em.createQuery("select o from Order o join o.member m" +
+                " where o.status = : status" +
+                " and m.username like :username", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("username", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList(); //Order 와 연관된 Member 를 조인하
+    }*/
+    /*
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch )
+        }
+    }*/
 }
