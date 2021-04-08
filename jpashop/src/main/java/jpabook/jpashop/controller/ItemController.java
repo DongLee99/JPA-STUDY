7de074ab8fd3ea713ca2becf25c2cddd405b13a8
DongLee99 <jhnj741@naver.com>
2021-04-08 20:20:19 +0900

    상품 조회 개발

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -2,12 +2,15 @@ package jpabook.jpashop.controller;
 
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +28,7 @@ public class ItemController {
     public String create(BookForm form){
 
         //나중에 create로 세터를 사용하지 말기
-        Book book = new Book();
+       Book book = new Book();
         book.setName(form.getName());
         book.setPrice(form.getPrice());
         book.setStockQuantity(form.getStockQuantity());
@@ -34,5 +37,12 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItem();
+        model.addAttribute("items",items);
+        return "items/itemList";
     }
 }
