1f36094c5602ea863454c77586c1e7d1ae3f813d
DongLee99 <jhnj741@naver.com>
2021-04-12 17:13:22 +0900

    DTO를 사용한 조회 Controller

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //@Controller @ResponseBody == RestController Date를 바로 json으로 보내자
 @RestController
@@ -19,6 +21,34 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(),collect);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { // RequestBody = json 데이타를 Member로 바꿔줌
         Long id = memberService.join(member);
@@ -33,7 +63,7 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
-// 실무에서 엔티티를 반환 X 엔티티 자체를 파라미터로 받기 X (ex : Member 를 그대로 사용하지 말고 Dto 를 사용하자)
+    // 실무에서 엔티티를 반환 X 엔티티 자체를 파라미터로 받기 X (ex : Member 를 그대로 사용하지 말고 Dto 를 사용하자)
     }
 
     @PutMapping("/api/v2/members/{id}")
