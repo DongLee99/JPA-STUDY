a6cc3fb06322b7f118ff7d370a15e6ab52cd8495
DongLee99 <jhnj741@naver.com>
2021-04-18 17:29:51 +0900

    주문 조회 v3.1 페이징

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -84,11 +84,34 @@ public class OrderRepository {
 
     }
 
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        List<Order> resultList = em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+        return resultList;
+
+    }
+
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery("select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, m.address) " +
                 " from Order o" +
                 " join o.member m" +
                 " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery("select distinct o from Order o" + //db에 distinct를 날려주고 order에 중복이 있으면 제거 ( sql에 distinct와는 다름.)
+                " join fetch o.member m" +
+                " join fetch o.delivery d" +
+                " join fetch o.orderItems oi" +
+                " join fetch oi.item i", Order.class)
+                .setFirstResult(1)
+                .setMaxResults(100)
                 .getResultList();
     }
 }
