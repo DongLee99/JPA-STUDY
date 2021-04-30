4778edc781ca6e1647c39a5bd1e666db804cf007
DongLee99 <jhnj741@naver.com>
2021-04-30 11:10:41 +0900

    스프링 데이터 JPA 사용

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -36,13 +36,13 @@ public class MemberService {
 
     @Transactional(readOnly = true)
     public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
 
     }
 
     @Transactional
     public Long update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
         return id;
     }
