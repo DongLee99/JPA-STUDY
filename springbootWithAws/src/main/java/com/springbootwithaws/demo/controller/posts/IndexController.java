0fdd91b6a0bccbb9ddcbbc18eb095960872b8c5c
DongLee99 <jhnj741@naver.com>
2021-09-27 15:35:29 +0900

    Test : IndexController

---
+++ springbootWithAws/src/main/java/com/springbootwithaws/demo/controller/posts/IndexController.java
@@ -0,0 +1,13 @@
+package com.springbootwithaws.demo.controller.posts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ springbootWithAws/src/test/java/com/springbootwithaws/demo/controller/posts/IndexControllerTest.java
@@ -0,0 +1,35 @@
+package com.springbootwithaws.demo.controller.posts;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지로딩() {
+        //given
+        //when
+        String body = this.restTemplate.getForObject("/",String.class);
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+    }
+
+}
